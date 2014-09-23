81ebe306170563f3abb6efc2d0257d702a554b62
whitepagec <support@whitepagecreation.com>
2014-09-23 18:05:44 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,37 @@
 <?php
- $test12345fjkgflgsf= "test 123";
- if($test12345fjkgflgsf != null){
- $test = 1;
- $test1 = "";
- 
-$array = [$test];
- }
+include 'test_1.php';
+function addFunction($num1, $num2)
+{
+  $sum = $num1 + $num2;
+  echo "Sum of the two numbers is : $sum";
+}
+addFunction(10, 20);
+
+function passFunction($num1, $num2)
+{
+  $sum = $num1 + $num2;
+  return $sum;
+}
+$return_value = passFunction(5, 20);
+echo "Returned value from the function : $return_value";
+
+if (strpos($return_value, "-") === false){
+  echo "false";
+}
+
+if (strpos($return_value, "-") === false){
+  echo "false";
+}
+
+if (strpos($return_value, "-") === false){
+  echo "false";
+}
+
+if (strpos($return_value, "-") === false){
+  echo "false";
+}
+
+if (strpos($return_value, "-") === false){
+  echo "false";
+}
 ?>
