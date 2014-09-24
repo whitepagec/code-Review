b881fe384df7102808ba5bb4bef997b0adb2078e
whitepagec <support@whitepagecreation.com>
2014-09-24 12:19:42 +0800

    Update test.php

--- test.php
+++ test.php
@@ -13,7 +13,7 @@ function passFunction($num1, $num2)
   return $sum;
 }
 $return_value = passFunction(5, 20);
-echo "Returned value from the function : $return_value";
+echo "Returned value from the function :" . $return_value;
 
 if (strpos($return_value, "-") === false){
   echo "false";
@@ -27,7 +27,7 @@ function xFunction($x1, $x2)
 {
   $xtotal = $x1+ $x2;
   $b = "not in use";
-  echo "Sum of the two numbers is :" $xtotal;
+  echo "Sum of the two numbers is :" . $xtotal;
 }
 xFunction(10);
 if (strpos($return_value, "-") === false){
