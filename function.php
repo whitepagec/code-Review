dc70a668b48dd8986ae9b0acdb16a12d92b34320
whitepagec <support@whitepagecreation.com>
2014-09-24 16:20:03 +0800

    Update function.php

--- function.php
+++ function.php
@@ -38,12 +38,12 @@ function read_line($fname,$subheader,$crumb,$content)
 	return ($html_content);
 }
 
-function simple_encrypt($text,$salt)
+function simple_encrypt_abc($text,$salt)
 {
     return trim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $salt, $text, MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND))));
 }
  
-function simple_decrypt($text,$salt)
+function simple_decrypt_def($text,$salt)
 {
 	return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $salt, base64_decode($text), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
 }
